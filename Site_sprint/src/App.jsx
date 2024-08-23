cbc78889a8cc63bfa79d51b82964a7ea45ab6d31
Vinícius Sales <viniciussales3210@gmail.com>
2024-08-22 22:32:58 -0300

    m

--- Site_sprint/src/App.jsx
+++ Site_sprint/src/App.jsx
@@ -2,12 +2,14 @@
 import React from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Main />
+      <Footer/>
     </div>
   );
 };
---
+++ Site_sprint/src/components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+// src/components/Footer.jsx
+import React from 'react';
+import './Footer.css';
+import logo from '/src/assets/images/logodouradasemfundo.png'; // Substitua pelo caminho correto da sua logo
+import instagramIcon from '/src/assets/images/instagram.png';
+import twitterIcon from '/src/assets/images/x.png';
+import facebookIcon from '/src/assets/images/facebook.png';
+import whatsappIcon from '/src/assets/images/whatsapp.png';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img className='Logo' src={logo} alt="Primezone Logo" />
+        </div>
+        <div className="footer-links">
+          <ul>
+            <li><a href="#login">Login</a></li>
+            <li><a href="#cadastro">Cadastro</a></li>
+            <li><a href="#ajuda">Central de ajuda</a></li>
+            <li><a href="#sobre">Quem somos</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <h4>Redes sociais</h4>
+          <div className="social-icons">
+            <a href="#instagram"><img src={instagramIcon} alt="Instagram" /></a>
+            <a href="#twitter"><img src={twitterIcon} alt="Twitter" /></a>
+            <a href="#facebook"><img src={facebookIcon} alt="Facebook" /></a>
+            <a href="#whatsapp"><img src={whatsappIcon} alt="Whatsapp" /></a>
+          </div>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>© Copyright 2024 Copyright.com.br - All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
