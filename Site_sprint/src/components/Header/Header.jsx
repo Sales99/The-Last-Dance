34a9df130d414f21513effc270e7002e5bec3db2
13Richard04 <richarddiezaraujo@gmail.com>
2024-08-29 16:20:46 -0300

    Toma sales na sua caraa
    sdadasd

--- Site_sprint/src/components/Header/Header.jsx
+++ Site_sprint/src/components/Header/Header.jsx
@@ -1,13 +1,14 @@
 // src/components/Header/Header.jsx
 import React from 'react';
 import './Header.css';
-
+import IconH from '../../assets/IconHeader';
 
 const Header = () => {
   return (
     <header className="header">
       <img src="/src/assets/images/logo.png" alt="Logo" className="logo" />
         <input type="text" placeholder="Procurar..." className="search-bar" />
+        <IconH/>
     </header>
   );
 };
