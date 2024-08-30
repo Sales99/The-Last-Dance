34f29e8351ab792064e38455f0f8e19d9cb6a1eb
13Richard04 <richarddiezaraujo@gmail.com>
2024-08-30 11:43:08 -0300

    j

--- Site_sprint/src/main.jsx
+++ Site_sprint/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './Pages/Home/App.jsx'
 import './index.css'
-import Chatbot from './components/Chatbot/Bot.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
