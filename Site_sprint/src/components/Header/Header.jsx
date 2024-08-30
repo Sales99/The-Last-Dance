c12f356761e22e4314ed22f9897d06c330384705
13Richard04 <richarddiezaraujo@gmail.com>
2024-08-30 11:47:16 -0300

    Commit do Salompa
    d

--- Site_sprint/src/components/Header/Header.jsx
+++ Site_sprint/src/components/Header/Header.jsx
@@ -8,9 +8,6 @@ const Header = () => {
     <header className="header">
       <img src="/src/assets/images/logo.png" alt="Logo" className="logo" />
         <input type="text" placeholder="Procurar..." className="search-bar" />
-        <ul>
-          <li><link to="/" className='link'></link></li>
-        </ul>
         <IconHeader/>
     </header>
   );
