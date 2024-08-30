abf4cfada1d2cdd39fb560db3f94d984a81c4fa8
Bruvini2006 <bruvini06@gmail.com>
2024-08-30 13:22:06 -0300

    sdfgtytrewsxcd
    srtyrhtefw

--- Site_sprint/src/Pages/LogarPage/LogarPage.jsx
+++ Site_sprint/src/Pages/LogarPage/LogarPage.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { collection, getFirestore, query, where, getDocs } from 'firebase/firestore';
-import { getAnalytics } from "firebase/analytics";
+import { getAnalytics } from 'firebase/analytics';
 import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
-import imagemTeste from '../../images/Imagem_ultra_prime.jpg';
+// import imagemTeste from '../../images/Imagem_ultra_prime.jpg';
 import './Logar.css';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
+// Configuração do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDYdvgk_Z3pYP-Lt_xpSwsvRyNghsSlS-4",
     authDomain: "teste-tcc-f1317.firebaseapp.com",
@@ -17,8 +18,9 @@ const firebaseConfig = {
     storageBucket: "teste-tcc-f1317.appspot.com",
     messagingSenderId: "174564069294",
     appId: "1:174564069294:web:045933f5e77bc0fd0a423f"
-  };
+};
 
+// Inicialização do Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth();
@@ -29,9 +31,13 @@ export function LogarPage() {
     const [senha, setSenha] = useState("");
     const [latestError, setLatestError] = useState("");
     const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
     const [showPassword, setShowPassword] = useState(false);
-    
+    const [resetPasswordMessage, setResetPasswordMessage] = useState('');
+    const [resetPasswordError, setResetPasswordError] = useState('');
+
+    const navigate = useNavigate();
+    const userCollectionRef = collection(db, 'users');
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -40,21 +46,19 @@ export function LogarPage() {
         });
 
         return () => unsubscribe();
-    }, [auth, navigate]);
-    
-    const userCollectionRef = collection(db, 'users');
+    }, [navigate]);
 
     const handleLogin = async (e) => {
         e.preventDefault();
         setLoading(true);
-    
+
         try {
             const q = query(userCollectionRef, where("email", "==", email));
             const querySnapshot = await getDocs(q);
-    
+
             if (!querySnapshot.empty) {
                 await signInWithEmailAndPassword(auth, email, senha);
-                navigate("/HomePage");
+                navigate("/Home");
             } else {
                 setLatestError("Usuário não encontrado. Por favor, verifique suas credenciais.");
             }
@@ -65,10 +69,10 @@ export function LogarPage() {
             setLoading(false);
         }
     };
-    
+
     const signInWithGoogle = async (e) => {
         e.preventDefault();
-    
+
         try {
             const provider = new GoogleAuthProvider();
             const result = await signInWithPopup(auth, provider);
@@ -79,10 +83,7 @@ export function LogarPage() {
             setLatestError("Erro ao fazer login com o Google. Por favor, tente novamente mais tarde.");
         }
     };
-    
-    const [resetPasswordMessage, setResetPasswordMessage] = useState('');
-    const [resetPasswordError, setResetPasswordError] = useState('');
-      
+
     const handleResetPassword = async (e) => {
         e.preventDefault();
         if (!email) {
@@ -104,7 +105,7 @@ export function LogarPage() {
             <br /><br /><br /><br />
             <div className="Login_container">
                 <div className="Login_Esquerda">
-                    <img src={imagemTeste} className="Imagem_Login" alt="Image_Login"></img>
+                    {/* <img src={imagemTeste} className="Imagem_Login" alt="Imagem Login" /> */}
                 </div>
                 <div className="Login_Direita">
                     <div className="Login_Titulo">
@@ -122,32 +123,32 @@ export function LogarPage() {
                                     required
                                     value={email}
                                     onChange={(e) => setEmail(e.target.value)}
-                                ></input>
+                                />
                             </div>
                             <div className="Inputs_senha">
-                            <label>Senha</label>
-                            <div className="Input_senha_container">
-                                <input
-                                type={showPassword ? "text" : "password"}
-                                className="Input_Inserir"
-                                id="Input_Senha"
-                                required
-                                placeholder="Insira sua senha"
-                                value={senha}
-                                onChange={(e) => setSenha(e.target.value)}
-                                />
-                                <div className="olho-icon" onClick={() => setShowPassword(!showPassword)}>
-                                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                <label>Senha</label>
+                                <div className="Input_senha_container">
+                                    <input
+                                        type={showPassword ? "text" : "password"}
+                                        className="Input_Inserir"
+                                        id="Input_Senha"
+                                        required
+                                        placeholder="Insira sua senha"
+                                        value={senha}
+                                        onChange={(e) => setSenha(e.target.value)}
+                                    />
+                                    <div className="olho-icon" onClick={() => setShowPassword(!showPassword)}>
+                                        {showPassword ? <FaEyeSlash /> : <FaEye />}
+                                    </div>
                                 </div>
-                            </div>
                             </div>
                         </div>
                         <div className="Opcoes_adicionais">
                             <div className="LembrarDeMim">
-                                <input type="checkbox" checked disabled></input>
+                                <input type="checkbox" checked disabled />
                                 <label>Lembrar de mim</label>
                             </div>
-                            <a className="Btn_EsqueceuSenha" onClick={handleResetPassword}>Esqueceu a senha?</a>
+                            <a href="#" className="Btn_EsqueceuSenha" onClick={handleResetPassword}>Esqueceu a senha?</a>
                         </div>
                         <div className="EnviarFormulario">
                             {loading ? (
@@ -156,23 +157,23 @@ export function LogarPage() {
                                 <button type="submit" className="Btn_EnviarFormulario">Enviar</button>
                             )}
                         </div>
-                    {/* Exibição da mensagem de erro após tentativa de login */}
-                    {latestError && <Alert variant="danger" className="Aviso_de_erro">{latestError}</Alert>}
-                    {/* Exibição da mensagem de sucesso ou erro após enviar email de redefinição */}
-                    {resetPasswordMessage && <Alert variant="info" className="Aviso_de_info">{resetPasswordMessage}</Alert>}
-                    {resetPasswordError && <Alert variant="danger" className="Aviso_de_erro">{resetPasswordError}</Alert>}
-                    <div className="Cadastre-se">
-                        <p className="Txt_Cadastro">Não tem uma conta?<Link to="../Cadastro" className="Btn_Cadastro">Cadastre-se</Link></p>
-                    </div>
-                    <div className="Login_Google">
-                        <p>Logar também com:</p>
-                        <div className="Fundo_Google">
-                            <a href="#" onClick={signInWithGoogle}><i className="bi bi-google"></i></a>
+                        {/* Exibição da mensagem de erro após tentativa de login */}
+                        {latestError && <Alert variant="danger" className="Aviso_de_erro">{latestError}</Alert>}
+                        {/* Exibição da mensagem de sucesso ou erro após enviar email de redefinição */}
+                        {resetPasswordMessage && <Alert variant="info" className="Aviso_de_info">{resetPasswordMessage}</Alert>}
+                        {resetPasswordError && <Alert variant="danger" className="Aviso_de_erro">{resetPasswordError}</Alert>}
+                        <div className="Cadastre-se">
+                            <p className="Txt_Cadastro">Não tem uma conta?<Link to="../Cadastro" className="Btn_Cadastro">Cadastre-se</Link></p>
                         </div>
-                    </div>
+                        <div className="Login_Google">
+                            <p>Logar também com:</p>
+                            <div className="Fundo_Google">
+                                <a href="#" onClick={signInWithGoogle}><i className="bi bi-google"></i></a>
+                            </div>
+                        </div>
                     </form>
                 </div>
             </div>
         </>
-    )
+    );
 }
