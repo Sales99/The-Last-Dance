7b31314ab61d1b386040b986660e713dd611f77d
13Richard04 <richarddiezaraujo@gmail.com>
2024-08-30 11:57:42 -0300

    asdasdasd
    asdsda

--- Site_sprint/src/Pages/Home/App.jsx
+++ Site_sprint/src/Pages/Home/App.jsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import Main from '../../components/Conteudo_Home/Main';
 import Footer from '../../components/Footer/Footer';
 import Chatbot from '../../components/Chatbot/Bot';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
   return (
