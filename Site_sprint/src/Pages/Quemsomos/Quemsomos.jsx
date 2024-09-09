36f2a37bfdb1d51f66c37da48b8821e7b12e635b
Vinícius Sales <viniciussales3210@gmail.com>
2024-09-09 19:59:41 -0300

    babinha

--- Site_sprint/src/Pages/Quemsomos/Quemsomos.jsx
+++ Site_sprint/src/Pages/Quemsomos/Quemsomos.jsx
@@ -25,6 +25,13 @@ const Quemsomos = () => {
             <p>
               Acreditamos que o sucesso vem de um atendimento personalizado, da utilização de tecnologias avançadas e da construção de relacionamentos de confiança.
             </p>
+            <p>
+            Somos a [Nome da Empresa], uma empresa dedicada a [área de atuação], comprometida com a excelência e inovação em tudo o que fazemos. Fundada em [ano de fundação], nossa missão é oferecer soluções de alta qualidade que atendam às necessidades de nossos clientes e parceiros. 
+
+Com uma equipe de profissionais experientes e apaixonados, buscamos transformar desafios em oportunidades, sempre com foco em resultados sustentáveis e no desenvolvimento contínuo.
+
+    Acreditamos que o sucesso vem de um atendimento personalizado, da utilização de tecnologias avançadas e da construção de relacionamentos de confiança. Nosso compromisso é com a transparência, a ética e a responsabilidade, garantindo que cada projeto seja conduzido com o mais alto padrão de qualidade.
+            </p>
           </div>
         </div>
       </main>
--- Site_sprint/src/components/Header/Header.jsx
+++ Site_sprint/src/components/Header/Header.jsx
@@ -2,11 +2,18 @@
 import React from 'react';
 import './Header.css';
 import IconHeader from '../../assets/Icons/IconHeader';
+import { Link } from 'react-router-dom'; // Importando o Link
+
 
 const Header = () => {
   return (
     <header className="header">
-      <img src="/src/assets/images/logo.png" alt="Logo" className="logo" />
+       <Link to="/"> 
+        <img src="/src/assets/images/logo.png" alt="Logo" className="logo" />
+      </Link>
+
+
+      
         <input type="text" placeholder="Procurar..." className="search-bar" />
         <IconHeader/>
     </header>
