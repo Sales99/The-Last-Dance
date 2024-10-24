80db8f265616b52e8ac3681d628356c1bfef04db
Vinícius Sales <viniciussales3210@gmail.com>
2024-10-24 16:09:59 -0300

    ererer

---
+++ Mobile_sprint/app/(tabs)/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Main from '../components/pages/Main';
+
+const App = () => {
+  return <Main />;
+};
+
+AppRegistry.registerComponent('meu-app', () => App);
+export default App;
--- Mobile_sprint/app/(tabs)/index.tsx
+++
@@ -1,7 +0,0 @@
-// index.tsx
-import React from 'react';
-import Main from '../(tabs)'
-
-export default function HomeScreen() {
-  return <Main />;
-}
